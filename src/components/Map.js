92ac7556a9162f12fc5c12c94104db76e8453b47
Calvin Kim <calvinkim1230@gmail.com>
2022-06-12 21:15:17 -0700

    changed map

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,7 +11,8 @@ const Map = () => {
       scrollWheelZoom={false}
     >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org">OpenMapTiles</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
       />
       <Marker position={[33.98053, -118.05089]}>
