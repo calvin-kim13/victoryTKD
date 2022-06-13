c2d37de902b7ca5a2b5dc7dd67b467a68a0a9795
Calvin Kim <calvinkim1230@gmail.com>
2022-06-12 21:27:53 -0700

    fixed map bug

--- src/components/Map.js
+++ src/components/Map.js
@@ -11,9 +11,9 @@ const Map = () => {
       scrollWheelZoom={false}
     >
       <TileLayer
-        // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org">OpenMapTiles</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        // attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org">OpenMapTiles</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png?api_key=6b9f1a70-d676-468b-b754-84a37d5e957a"
       />
       <Marker position={[33.98053, -118.05089]}>
         <Popup>Victory Taekwondo (V1)</Popup>
